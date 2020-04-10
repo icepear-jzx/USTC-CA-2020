c6792c130008120cbecf229c11841cc7f7c37189
IcePear-Jzx <icepear.jzx@gmail.com>
2020-04-10 15:18:29 +0800

    test0

--- Lab2/Simulation/insert_nop.py
+++
@@ -1,8 +0,0 @@
-fin = open('1testAll.inst', 'r')
-fout = open('1testAll.bubble', 'w')
-for line in fin:
-    fout.write(line)
-    fout.write("00000000\r\n")
-    fout.write("00000000\r\n")
-    fout.write("00000000\r\n")
-    fout.write("00000000\r\n")
