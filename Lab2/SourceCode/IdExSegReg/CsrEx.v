fe0b6de613148f62645a22c287f13f32f0d5e526
IcePear-Jzx <icepear.jzx@gmail.com>
2020-04-15 00:53:54 +0800

    fix bug

--- Lab2/SourceCode/IdExSegReg/CsrEx.v
+++ Lab2/SourceCode/IdExSegReg/CsrEx.v
@@ -2,11 +2,13 @@
 
 module Csr_EX(
     input wire clk, bubbleE, flushE,
+    input wire [1:0] Mask_func_ID,
     input wire csr_read_en_ID,
     input wire csr_write_en_ID,
     input wire [11:0] csr_dest_ID,
     input wire [31:0] csr_out_ID,
     input wire [31:0] reg1_or_zimm_ID,
+    output reg [1:0] Mask_func_EX,
     output reg csr_read_en_EX,
     output reg csr_write_en_EX,
     output reg [11:0] csr_dest_EX,
@@ -16,6 +18,7 @@ module Csr_EX(
 
     initial 
     begin
+        Mask_func_EX = 0;
         csr_read_en_EX = 0;
         csr_write_en_EX = 0;
         csr_dest_EX = 0;
@@ -28,6 +31,7 @@ module Csr_EX(
         begin
             if (flushE)
             begin
+                Mask_func_EX <= 0;
                 csr_read_en_EX <= 0;
                 csr_write_en_EX <= 0;
                 csr_dest_EX <= 0;
@@ -36,6 +40,7 @@ module Csr_EX(
             end
             else 
             begin
+                Mask_func_EX <= Mask_func_ID;
                 csr_read_en_EX <= csr_read_en_ID;
                 csr_write_en_EX <= csr_write_en_ID;
                 csr_dest_EX <= csr_dest_ID;
